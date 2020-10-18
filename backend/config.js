4e782132db3a8195055e30bf633815a3cce67cc2
f-ke <1945334800@qq.com>
2020-10-19 06:41:03 +0800

    json edit

--- backend/config.js
+++ backend/config.js
@@ -4,6 +4,6 @@ export default{
     MONGODB_URL : process.env.MONGODB_URL||'mongodb://localhost/amazon',
     JWT_SECRET : process.env.JWT_SECRET||'jwtsecret',
     PAYPAL_CLIENT_ID:process.env.PAYPAL_CLIENT_ID||'sb',
-    PORT: process.env.PORT||5000
+    PORT: process.env.PORT || 5000
 }
 // .env->config
