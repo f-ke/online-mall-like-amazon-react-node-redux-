2f670f89fe4ca1a3baf6b6d87af689ce68c558b7
f-ke <1945334800@qq.com>
2020-10-21 05:38:16 +0800

    try2

--- backend/server.js
+++ backend/server.js
@@ -51,14 +51,15 @@ app.get("/api/config/paypal",(req,res)=>{
 //     next();
 //   });
   
-if(process.env.NODE_ENV ==='production'){
+/*if(process.env.NODE_ENV ==='production'){
    app.use(express.static('../frontend/build'))
  }
+ */
 
-//app.use(express.static(path.join(__dirname, '/../frontend/build')));
-/*app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
-});*/
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.get('*', (req, res) => {
+ res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+ });
 app.listen(config.PORT, ()=>{
     console.log("server start sucessfully at localhost:5000");
 }
